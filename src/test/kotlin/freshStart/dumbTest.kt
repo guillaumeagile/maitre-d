b93faf58d8428603e07ba8528bf945713fb61e50
g.saintetienne <g.saintetienne@primobox.com>
2021-05-14 20:35:50 +0200

    added simple test just to try the test libs

---
+++ src/test/kotlin/freshStart/dumbTest.kt
@@ -0,0 +1,24 @@
+package freshStart
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+
+class dumbTest : StringSpec({
+
+    "fetchUser should return name and id" {
+        val user = fetchUser("sam")
+        user.name shouldBe "sam"
+        user.id shouldBe "123"
+    }
+})
+
+fun fetchUser(s: String): User {
+    return User("sam", "123")
+}
+
+class User(name: String, id: String) {
+
+    val name: String = name
+    val id: String = id
+}
