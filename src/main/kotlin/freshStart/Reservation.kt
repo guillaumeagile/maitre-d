0acdd540c36089b66f95836e0d5518957161e9f8
g.saintetienne <g.saintetienne@primobox.com>
2021-05-21 20:03:06 +0200

    second test:  should return a failure

--- src/main/kotlin/freshStart/Reservation.kt
+++ src/main/kotlin/freshStart/Reservation.kt
@@ -2,6 +2,10 @@ package freshStart
 
 import java.time.LocalDate
 
+fun create( date: LocalDate,  quantity: Int): Result<Reservation>
+{
+    return Result.success(Reservation(date, quantity))
+}
 
  class Reservation(val Date: LocalDate, val Quantity: Int) {
 
---
+++ src/test/kotlin/freshStart/ReservationTest.kt
@@ -0,0 +1,24 @@
+package freshStart
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import java.time.LocalDate
+import java.time.Month
+
+
+class TestThatReservation : StringSpec({
+
+    "Should have a date and a quantity" {
+        val reservation = Reservation(LocalDate.of(1990, Month.DECEMBER, 15), 1)
+        reservation.Date shouldBe LocalDate.of(1990, Month.DECEMBER, 15)
+        reservation.Quantity shouldBe 1
+    }
+
+    "Should not have  a quantity  equal to 0" {
+        val resultat =  create (LocalDate.of(1990, Month.DECEMBER, 15), 0)
+
+        resultat.isFailure shouldBe true
+    }
+})
+
+
