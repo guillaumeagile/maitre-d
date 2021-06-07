aed1dcd632a146476ed0609d7156fec5cda9e1c6
g.saintetienne <g.saintetienne@primobox.com>
2021-06-07 19:27:39 +0200

    2 resa la meme journée dépassant la capacité max

--- src/main/kotlin/freshStart/Reservation.kt
+++ src/main/kotlin/freshStart/Reservation.kt
@@ -3,17 +3,17 @@ package freshStart
 import java.time.LocalDate
 
 
-
 class Reservation(val date: LocalDate, val quantity: Quantity) {
     companion object {
+        var accumulation: Int = 0
         fun create(date: LocalDate, quantity: Int, table: Table): Result<Reservation> {
             val qtt = Quantity.create(quantity)
-            return qtt.fold({q ->
-                                TODO("enlever ce if (essayer un 2e fold)")
-                                if (q.value_ > table.size)
-                                    return Result.failure(NoRoomLeft())
-                                return Result.success(Reservation(date, q))
-                            },
+            return qtt.fold({ q ->
+                if (q.value_ + accumulation > table.size )
+                    return Result.failure(NoRoomLeft())
+                accumulation += q.value_
+                return Result.success(Reservation(date, q))
+            },
                 { _ -> Result.failure(InvalidQuantityForReservation()) }
             )
         }
--- src/test/kotlin/freshStart/TestThatReservation.kt
+++ src/test/kotlin/freshStart/TestThatReservation.kt
@@ -15,26 +15,36 @@ class TestThatReservation : StringSpec({
     }
 
     "Should not have  a quantity  equal to 0" {
-        val resultat =  Reservation.create (LocalDate.of(1990, Month.DECEMBER, 15), 0, Table(1))
+        val resultat = Reservation.create(LocalDate.of(1990, Month.DECEMBER, 15), 0, Table(1))
         resultat.isFailure shouldBe true
     }
 
     "Should have not a quantity more than the table capacity (12)" {
-        val resultat =  Reservation.create (LocalDate.of(1990, Month.DECEMBER, 15), 13, Table(12)        )
+        val resultat = Reservation.create(LocalDate.of(1990, Month.DECEMBER, 15), 13, Table(12))
         resultat.isFailure shouldBe true
         resultat.onFailure { e -> e is NoRoomLeft }
     }
 
     "Should have not a quantity more than the table capacity (4)" {
-        val resultat =  Reservation.create (LocalDate.of(1990, Month.DECEMBER, 15), 5, Table(4)        )
+        val resultat = Reservation.create(LocalDate.of(1990, Month.DECEMBER, 15), 5, Table(4))
         resultat.isFailure shouldBe true
         resultat.onFailure { e -> e is NoRoomLeft }
     }
 
     "Should have not a negative quantity " {
-        val resultat =  Reservation.create (LocalDate.of(1990, Month.DECEMBER, 15), -2, Table(-1)        )
+        val resultat = Reservation.create(LocalDate.of(1990, Month.DECEMBER, 15), -2, Table(-1))
         resultat.isFailure shouldBe true
         resultat.onFailure { e -> e is InvalidQuantityForReservation }
     }
+
+    "Should have not a quantity more than the table capacity (4) the same day" {
+        val table = Table(4)
+        Reservation.create(LocalDate.of(1990, Month.DECEMBER, 15), 2, table )
+        val resultat = Reservation.create(LocalDate.of(1990, Month.DECEMBER, 15), 3, table)
+        resultat.isFailure shouldBe true
+        resultat.onFailure { e -> e is NoRoomLeft }
+
+    }
+
 
 })
