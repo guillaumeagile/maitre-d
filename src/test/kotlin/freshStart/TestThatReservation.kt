85aef52c1dd6835ee2b67f9283e1a45646b9f63b
g.saintetienne <g.saintetienne@primobox.com>
2021-07-02 19:13:07 +0200

    cas limite ok

--- src/test/kotlin/freshStart/TestThatReservation.kt
+++ src/test/kotlin/freshStart/TestThatReservation.kt
@@ -13,28 +13,28 @@ class TestThatReservation : StringSpec({
 
     "Should have a date and a quantity" {
         val maitreD = MaitreD( 1)
-        var actualReservation = maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), 1).getOrThrow()
+        val actualReservation = maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), 1).getOrThrow()
         actualReservation.date shouldBe LocalDate.of(1990, Month.DECEMBER, 15)
         actualReservation.quantity shouldBe 1
     }
 
     "Should not have  a quantity  equal to 0" {
         val maitreD = MaitreD( 1)
-        var actualReservation = maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), 0)
+        val actualReservation = maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), 0)
         actualReservation.isFailure shouldBe true
         actualReservation.onFailure { e ->  e should beInstanceOf<InvalidQuantityForReservation>() }
     }
 
     "Should have not a quantity more than the table size (12)" {
         val maitreD = MaitreD( 12)
-        var actualReservation = maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), 13)
+        val actualReservation = maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), 13)
         actualReservation.isFailure shouldBe true
         actualReservation.onFailure { e ->  e should beInstanceOf<NoRoomLeft>() }
     }
 
     "Should have not a negative quantity " {
         val maitreD = MaitreD( -1)
-        var actualReservation = maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), -2)
+        val actualReservation = maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), -2)
         actualReservation.isFailure shouldBe true
         actualReservation.onFailure { e ->  e should beInstanceOf<InvalidQuantityForReservation>() }
     }
@@ -44,14 +44,15 @@ class TestThatReservation : StringSpec({
         maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), 3)
         val actualReservation = maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), 2)
         actualReservation.isFailure shouldBe true
+        actualReservation.onFailure { e ->  e should beInstanceOf<NoRoomLeft>() }
     }
 
     "Should have  a quantity enough for the same day" {
-        val table = Table(10)
-        Reservation.create(LocalDate.of(1990, Month.DECEMBER, 15), 3, table )
-        val resultat = Reservation.create(LocalDate.of(1990, Month.DECEMBER, 15), 3, table)
-        resultat.isFailure shouldBe false
-        resultat.getOrNull()?.quantity shouldBe 3
+        val maitreD = MaitreD( 10)
+        maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), 3)
+        val actualReservation = maitreD.reserve(LocalDate.of(1990, Month.DECEMBER, 15), 7)
+        actualReservation.isFailure shouldBe false
+        actualReservation.getOrThrow().quantity shouldBe 7
     }
 
     "Should have a quantity enough for two different day" {
