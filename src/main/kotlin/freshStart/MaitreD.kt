cfb67e828129789d94c3682c68d970bfc5b68e21
g.saintetienne <g.saintetienne@primobox.com>
2021-08-30 11:02:18 +0200

    refacto

--- src/main/kotlin/freshStart/MaitreD.kt
+++ src/main/kotlin/freshStart/MaitreD.kt
@@ -4,25 +4,19 @@ import freshStart.tables.HauteCuisineTable
 import java.time.LocalDate
 
 class MaitreD(val tables: MutableList<ITable>) {
-    var dailySeatsOverallReservations = DailySeats()
 
     fun reserve(date: LocalDate, numberOfGuests: Int): Result<Reservation> {
         val resultQuantity = Quantity.create(numberOfGuests)
         var lastObservedTable: ITable? = null;
         return resultQuantity.fold(
             { quantity ->
-                val quantityOfReservedSeat =
-                    dailySeatsOverallReservations.howManyReservedOn(date) + quantity   //TODO: ne plus utiliser
                 var reservation: Reservation? = null
                 for ((index, currentTable) in tables.withIndex()) {
                     lastObservedTable = currentTable
                     if (!currentTable.canIReserve(date, quantity))
                         continue
-                    dailySeatsOverallReservations =
-                        dailySeatsOverallReservations.addReservation(date, quantity)   //TODO: ne plus utiliser
-                    reservation = Reservation(date, quantity)   //TODO: ne plus utiliser
+                    reservation = Reservation(date, quantity)   //TODO: ne plus utiliser ?
                     tables[index] = currentTable.reserve(date, quantity)
-
                     break
                 }
                 if (reservation != null)
