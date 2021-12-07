bb91089107cb6cdf9451f134b581acd3b68aa89b
g.saintetienne <g.saintetienne@primobox.com>
2021-12-07 18:11:01 +0100

    TODO: inverser les responsabilités pour la fonction de décision

--- src/main/kotlin/freshStart/MaitreD2.kt
+++ src/main/kotlin/freshStart/MaitreD2.kt
@@ -8,10 +8,13 @@ import freshStart.events.ReservationIsDeclinedOnSharedTable
 import freshStart.events.ReservationIsProposedOnSharedTable
 
 class MaitreD2(val sharedTableInInitialState: SharedTable) {
-    var events: Collection<Event> = setOf<Event>() // TODO : avoir un getter readonly
+
+    var events: Collection<Event> = setOf<Event>()
+
     fun handle(command: ICommand) {
         val sharedTableCurrentState = sharedTableInInitialState.replayOn(listEvents = events)
-        if (command.isValidReservation(sharedTableCurrentState)) {
+        if (command.isValidReservation(sharedTableCurrentState)) { // FIXME: ce n'est pas à la commande de valider la réservation
+            //  mais plutôt à la table de valider la commande
             sharedTableCurrentState.reserve(
                 date = (command as ReservationCommand).wishedDate, // éviter le cast pas beau et respecter la loi de Demeter
                 qtte = Quantity(command.guestsCount) // respecter la loi de Demeter
@@ -24,7 +27,8 @@ class MaitreD2(val sharedTableInInitialState: SharedTable) {
                     qtte = Quantity(command.guestsCount)
                 )
             )
-        } else
+        } else {
             events = events.plus(ReservationIsDeclinedOnSharedTable(reservationNumber = 1))
+        }
     }
 }
