ceada5615cd0514a056a5ba5493595b36d893683
g.saintetienne <g.saintetienne@primobox.com>
2021-12-08 22:55:42 +0100

    Idée:  versionner la ressource pour éviter les erreurs de collisions entre la fonction de décision et l'enregistrement des évènements (race condition)

--- src/main/kotlin/freshStart/MaitreD2.kt
+++ src/main/kotlin/freshStart/MaitreD2.kt
@@ -16,16 +16,15 @@ class MaitreD2(val sharedTableInInitialState: SharedTable) {
 
         val sharedTableCurrentState = sharedTableInInitialState.replayOn(listEvents = events)
         val reservationCommand = (command as ReservationCommand)
-        // IDEE: donner une version de la ressource à modifer
+        // IDEE: toujours avoir une version de la ressource à modifier    https://event-driven.io/en/optimistic_concurrency_for_pessimistic_times/
         if (sharedTableCurrentState.canIReserve(
                 date = reservationCommand.wishedDate,
                 qtte = Quantity(reservationCommand.guestsCount)
             )
         ) {
-            // que se passerait il si un autre évènement similaire passe avant celui ci? (accès concurrent)
-            // pour s'en prémunir, on incrémente la version de la ressource dans l'évènement au moment de
-            // l'enregistrment de l'event, et on verifié qu' il n'existe une version égale déjà présente dans les 
-            // évènements passés
+            // que se passerait il si un autre évènement de mutation passe avant celui ci? (accès concurrent)
+            // pour s'en prémunir, on vérifie que la version de la ressource dans le dernier évènement enregistré est le même
+            // si OK, on change ce numéro de version juste au moment où on enregistre l'évènement de mutation
             events = events.plus(
                 ReservationIsConfirmedOnSharedTable(
                     reservationNumber = 1,
@@ -33,8 +32,8 @@ class MaitreD2(val sharedTableInInitialState: SharedTable) {
                     qtte = Quantity(command.guestsCount)
                 )
             )
-            // l'ajout de l'évènement sera être rejeté si un autre evenement de même type
-            // a été trouvé pour le même ID de ressource avec une version égale ou supérieure
+            // l'ajout de l'évènement sera être rejeté si un autre évènement de mutation a été trouvé pour le même ID de ressource
+            // mais portant sur une version différente de la version lue au moment du replayOn (fonction de projection)
         } else {
             events = events.plus(ReservationIsDeclinedOnSharedTable(reservationNumber = 1))
         }
